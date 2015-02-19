3a0cf299f781712b0f037aca17ab99422bb4bebc
Alejandro Tardin <alejandro@tardin.com>
2015-02-19 11:14:37 +0000

    Fixed templates javascript output. It was not valid JS as it contained an extra bracket at the end.

--- tasks/ractiveparse.js
+++ tasks/ractiveparse.js
@@ -68,8 +68,16 @@
             templateFile = file.templateFile;
 
             // Join parsed files and write them to a new file.
-            grunt.file.write(file.dest,
-                options.destSyntax + " {" + templateOptions + templates.join(",\n") + "\n\n});");
+            var content = options.destSyntax +
+            " {" + templateOptions + templates.join(",\n") + "\n\n}";
+
+            console.log(options.type);
+            if (options.type === 'extjs') {
+                content += ')';
+            }
+            content += ';';
+
+            grunt.file.write(file.dest, content);
 
             // Log success.
             grunt.log.writeln('File "' + chalk.cyan(file.dest) + '" created.');
